d2343c9935fb82c868150379b5fdf2e15dd8a2ba
elboqueronpaco <elboqueronpaco@gmail.com>
2022-11-01 11:30:02 +0100

    swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 
+@ApiTags('auth')
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('API NestJS Curso')
+    .setDescription('Esta es la api del curso NestJs')
+    .addTag('courses')
+    .addTag('videos')
+    .addTag('awards')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
